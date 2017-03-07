7832a3071fa584f739e5d5da8203e0a6d4293bb4
antsa <r.antsa.fitiavana@gmail.com>
2017-03-07 09:55:30 +0400

    commit after clustering done

--- clustering.py
+++ clustering.py
@@ -6,10 +6,10 @@ from mongoengine import *
 from pymongo import MongoClient
 from article import NewArticle
 from urls import Url
-from . import lib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import MeanShift
+from lib import *
 
 class GroupCluster(Document):
     cluster_number = IntField()
@@ -17,14 +17,15 @@ class GroupCluster(Document):
     meta = {'collection':'clusters','strict':False}
 
 stopwords = nltk.corpus.stopwords.words('french')
-content = lib.get_content_article()
-tokenize_only = lib.tokenize_only(content.values())
-
-# test ken
+content = get_content_article()
+#print(content.values())
+#tokenize_only = tokenize_only(content.values())
 
 #tf-idf representation
 tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000, min_df=0.2, stop_words=stopwords, use_idf=True, tokenizer=tokenize_only, ngram_range=(1,3))
 tfidf_matrix = tfidf_vectorizer.fit_transform(content.values())
+
+print('TF-IDF done')
 #print(tfidf_matrix.shape)
 terms = tfidf_vectorizer.get_feature_names()
 
@@ -34,9 +35,9 @@ ms = MeanShift()
 ms.fit(dist)
 clusters = ms.labels_.tolist()
 cluster_centers = ms.cluster_centers_
-
+print('We actually have %d clusters' %len(cluster_centers))
 #storage of the cluster_id with the article_id in them in a new collection, named Event
-#must create the collection Event first
+
 if connect('azotData'):
     for cl in clusters:
 	cluster = GroupCluster()
@@ -44,9 +45,8 @@ if connect('azotData'):
     for val in content.keys():
 	cluster = GroupCluster()
 	cluster.article_id = val
-#test commit    
+    print('Storage of clusters done')
 #get the keywords per cluster (and add the keywords in the new collection Event)
 
-# test ken
 
      
